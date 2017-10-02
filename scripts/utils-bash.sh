57aaef2ddafa7c7781e8121ec530d47498c04c6d
ddesvillechabrol <dimitri.desvillechabrol@curie.fr>
2017-10-02 17:56:27 +0200

    [ADD] add run_bash function; add get_json_entry

--- scripts/utils-bash.sh
+++ scripts/utils-bash.sh
@@ -18,9 +18,42 @@
 # not exit.
 # Inputs:
 #       - String $1: Directory to create.
-create_directory()
-{
+#
+create_directory(){
     if [ ! -d "${1}" ]; then
         mkdir "${1}"
+    fi
+}
+
+# ----------------------------------------------------------------------
+# Function to gets json entry and to remove leading and trailing quote.
+# Inputs:
+#       - String $1: Entry of JSON file like ".default.threads"
+#       - String $2: JSON config file.
+#
+get_json_entry(){
+    jq_output="$(jq "${1}" ${2})"
+    echo $(sed -e 's/^"//' -e 's/"$//' <<<"${jq_output}")
+}
+
+# ----------------------------------------------------------------------
+# 
+#
+
+# ----------------------------------------------------------------------
+# Function to run bash as qsub or not.
+# Inputs:
+#       - String $1: Command line to run.
+#       - String $2: JSON config file.
+#       - Int $3: PID of a previous job.
+#
+run_bash(){
+    local cmd=($1)
+    local config=$2
+    local pid=${3:-foo}
+    if [[ -n ${CLUSTER} ]]; then
+        echo OK       
+    else 
+        echo $(eval "bash ${cmd[@]} $config")
     fi
 }
