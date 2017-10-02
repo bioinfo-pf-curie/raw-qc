46cb736af5880290dcfa803b2bdbadfcaef7ad50
ddesvillechabrol <dimitri.desvillechabrol@curie.fr>
2017-10-02 17:30:20 +0200

    [BUGFIX] fix typo error

--- scripts/fastqc-bash.sh
+++ scripts/fastqc-bash.sh
@@ -13,44 +13,51 @@
 #- software.
 #- ---------------------------------------------------------------------
 
-## ----------------------------------------------------------------------
+## ---------------------------------------------------------------------
 ## Fastqc
 ##
-## Wrapper of Fastqc with many sanity check.
+## Wrapper of Fastqc with many sanity check. It creates a HTML file
+## with quality control of FASTQ file(s).
 ## Inputs:
 ##      - String $1: Fastq file(s).
 ##      - String $2: Output directory.
+##      - String $3: JSON configuration file.
 ## ---------------------------------------------------------------------
 
 fastq=($1)
 output=$2
+config=$3
+
+# Import utils
+path="${0%/*}/"
+. "${path}"utils-bash.sh
 
 # Catch variable in json
-fastqc_path="$(jq ".fastqc.path" ${CONFIG})"
-fastqc_option="$(jq ".fastqc.options" ${CONFIG})"
-fastqc_threads="$(jq ".fastqc.threads" ${CONFIG})"
+fastqc_path="$(get_json_entry ".fastqc.path" ${config})"
+fastqc_option="$(get_json_entry ".fastqc.options" ${config})"
+fastqc_threads="$(get_json_entry ".fastqc.threads" ${config})"
 
 # Set some local variable
 name=${fastq[0]%.fastq*}
-fastqc_dir="${output}/fastqc/"
-create_directory $fastqc_dir
-fastqc_zip_file="${fastqc_dir}${name}_fastqc.zip"
+create_directory $output
+fastqc_zip_file="${output}${name}_fastqc.zip"
 
-# if the zip file already exists
-# we have nothing to do
-test -d ${fastqc_dir} && test -f ${fastqc_zip_file} && test ${fastqc_zip_file} -nt ${fastq[0]} && exit 0
+# if the zip file already exists we have nothing to do
+test -d ${output} && test -f ${fastqc_zip_file} && test ${fastqc_zip_file} -nt ${fastq[0]} && exit 0
 
 # if the file is empty, we don't need to do anything
-test -s $fq || exit 0 
+test -s ${fastq[0]} || exit 0 
 
 # seems that fastqc seldomly failed.
 _fail=0
-cmd="${FASTQC_PATH}fastqc ${FASTQC_OPT} ${fastq[@]} --threads ${fastqc_threads} --outdir ${fastqc_dir}"
+cmd="${fastqc_path}fastqc ${fastqc_opt} ${fastq[@]} \
+                          --threads ${fastqc_threads} \
+                          --outdir ${output}"
 $cmd || _fail=1
 
 # retry once in case of failure
 test ${_fail} -eq 0 || { _fail=0; $cmd; } || _fail=1
-test ${_fail} -eq 1 || test -d ${fastqc_dir} || _fail=1
-test ${_fail} -eq 0 || rm -rf ${fastqc_dir} ${fastqc_zip_file}
+test ${_fail} -eq 1 || test -d ${output} || _fail=1
+test ${_fail} -eq 0 || rm -rf ${output} ${fastqc_zip_file}
 
 exit ${_fail}
