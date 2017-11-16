c95dc75039614e624f903d469e0c6e35f88eda64
ddesvillechabrol <dimitri.desvillechabrol@curie.fr>
2017-11-16 17:04:45 +0100

    [MODIF] add metrics and json file creation

--- scripts/autotropos-bash.sh
+++ scripts/autotropos-bash.sh
@@ -19,8 +19,8 @@
 ## Wrapper of Autotropos with many sanity check. It creates FASTQ
 ## file(s) with adapters removed.
 ## Inputs:
-##      - String $INPUT: Input FASTQ file(s). (R1 (R2)?)
-##      - String $OUTPUT: Output FASTQ file(s). (R1 (R2)?)
+##      - String $INPUT: Input FastQ file(s).
+##      - String $OUTPUT: Output FastQ file(s), metrics file and json file.
 ##      - String $PLAN: CSV file of sample plan.
 ##      - String $CONFIG: JSON configuration file.
 ##      - String $LOG: Log file.
@@ -33,6 +33,15 @@ path="${BASH_SOURCE[0]%/*}/"
 # Initiate variable of the wrapper ($INPUT, $OUTPUT, $PLAN, $CONFIG, $LOG)
 init_wrapper $@
 
+# Catch variable in json
+autotropos_path="$(get_json_entry ".autotropos.path" ${CONFIG})"
+autotropos_option="$(get_json_entry ".autotropos.options" ${CONFIG})"
+autotropos_threads="$(get_json_entry ".autotropos.threads" ${CONFIG})"
+
+if [[ -n "${autotropos_path}" ]]; then
+    autotropos_path="${autotropos_path%/}/"
+fi
+
 # Check if task is launch as job array
 if [[ -n ${PBS_ARRAYID} ]]; then
     sample_array=($(get_sample $PLAN ${PBS_ARRAYID}))
@@ -43,24 +52,21 @@ fi
 
 fastq_input=("${sample_array[@]:1:2}")
 # Add ID name in outputs and logs
-fastq_output=($(populate_template "${OUTPUT}" ${sample_array[0]}))
+outputs=($(populate_template "${OUTPUT}" ${sample_array[0]}))
+create_directory ${outputs[0]%/*}
 log_output=$(populate_template "${LOG}" ${sample_array[0]})
 create_directory ${log_output%/*}
 
-# Catch variable in json
-autotropos_path="$(get_json_entry ".autotropos.path" ${CONFIG})"
-autotropos_option="$(get_json_entry ".autotropos.options" ${CONFIG})"
-autotropos_threads="$(get_json_entry ".autotropos.threads" ${CONFIG})"
-if [[ -n "${autotropos_path}" ]]; then
-    autotropos_path="${autotropos_path%/}/"
-fi
+# set atropos logs and autotropos json
+sub_index=$((${#outputs[@]} - 2))
+autotropos_utils=(${outputs[@]:$sub_index:2})
 
 # Set some local variable
 autotropos_input=("-1" "${fastq_input[0]}")
-autotropos_output=("-o" "${fastq_output[0]}")
-if [[ ${#fastq_input[@]} -eq 2 && ${#fastq_output[@]} -eq 2 ]]; then
+autotropos_output=("-o" "${outputs[0]}")
+if [[ ${#fastq_input[@]} -eq 2 ]]; then
     autotropos_input+=("-2" "${fastq_input[1]}")
-    autotropos_output+=("-p" "${fastq_output[1]}")
+    autotropos_output+=("-p" "${outputs[1]}")
 fi
 _fail=0 # variable to check if everything is ok 
 
@@ -68,7 +74,9 @@ _fail=0 # variable to check if everything is ok
 cmd="${autotropos_path}autotropos ${autotropos_option[@]} \
                                   --threads ${autotropos_threads} \
                                   ${autotropos_input[@]} \
-                                  ${autotropos_output[@]}"
+                                  ${autotropos_output[@]} \
+                                  --logs ${autotropos_utils[0]} \
+                                  --json ${autotropos_utils[1]}"
 echo $cmd > ${log_output}
 $cmd >> "${log_output}" 2>&1 || _fail=1
 
