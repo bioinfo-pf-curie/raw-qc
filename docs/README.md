eef2e2f10f0cc56aebe01982be73a40e99dbbba8
Frederic Jarlier <frederic.jarlier@curie.fr>
2020-10-13 11:22:01 +0200

    Please enter the commit message for your changes. Lines starting

--- docs/README.md
+++
@@ -1,8 +0,0 @@
-# Documentation
-
-The documentation is split into the following files:
-
-1. [Installation](installation.md)
-2. [Running the pipeline](usage.md)
-3. [Output and how to interpret the results](output.md)
-4. [Troubleshooting](troubleshooting.md)
--- install/cmake-init.cmake
+++
@@ -1,6 +0,0 @@
-set(CMAKE_INSTALL_PREFIX "/bioinfo/local/curie/ngs-data-analysis/centos/rawqc" CACHE PATH "")
-set(ap_nf_executor "pbs" CACHE STRING "")
-set(ap_singularity_image_path "/bioinfo/local/curie/ngs-data-analysis/centos/tools/containers/singularity/rawqc-2.0/images" CACHE PATH "")
-set(ap_tools_path "/to/replace if ap_use_toolspath is ON" CACHE PATH "")
-set(ap_use_containers "ON" CACHE BOOL "")
-set(ap_use_toolspath "OFF" CACHE BOOL "")
